e50b68957bbf54a8bcce33afb4585fb0ca9a37b8
Antonio Reyes <antonioreyesrabel.guadalupe@alumnado.fundacionloyola.net>
2024-01-11 20:59:52 +0100

    actores en tabla, si no gusta ir al comit anterios

--- views/vlistarautor.js
+++ views/vlistarautor.js
@@ -9,18 +9,51 @@ export class ListarAutor extends Vista {
 
     async visualizarAutor() {
         const autores = await this.datos.mostrarAutor();
-
+    
         if (autores) {
-            const divListarAutor = document.getElementById('divListarAutor');
-            divListarAutor.innerHTML = ''; // Limpiamos el contenido actual
-
+            const tablaAutores = document.getElementById('tabla_autores');
+            tablaAutores.innerHTML = ''; // Limpiamos el contenido actual
+    
             autores.forEach((autor) => {
-                const autoresDiv = document.createElement('div');
-                autoresDiv.textContent = `Nombre: ${autor.nombre}, Fecha Nacimiento: ${autor.fecha_nacimiento}, Fecha Muerte: ${autor.fecha_muerte}, Nacionalidad: ${autor.nacionalidad}, Foto: ${autor.foto}`;
-                divListarAutor.appendChild(autoresDiv);
+                const filaAutor = document.createElement('tr');
+                
+                const nombreTD = document.createElement('td');
+                nombreTD.textContent = autor.nombre;
+                filaAutor.appendChild(nombreTD);
+    
+                const fechaNacimientoTD = document.createElement('td');
+                fechaNacimientoTD.textContent = autor.fecha_nacimiento;
+                filaAutor.appendChild(fechaNacimientoTD);
+    
+                const fechaMuerteTD = document.createElement('td');
+                fechaMuerteTD.textContent = autor.fecha_muerte;
+                filaAutor.appendChild(fechaMuerteTD);
+    
+                const fotoTD = document.createElement('td');
+                fotoTD.textContent = autor.foto; // O puedes usar un elemento de imagen si 'foto' es la URL de la imagen
+                filaAutor.appendChild(fotoTD);
+    
+                const nacionalidadTD = document.createElement('td');
+                nacionalidadTD.textContent = autor.nacionalidad;
+                filaAutor.appendChild(nacionalidadTD);
+    
+                const borrarTD = document.createElement('td');
+                const imgBorrar = document.createElement('img');
+                imgBorrar.src = 'imagenes/basura.png';
+                borrarTD.appendChild(imgBorrar);
+                filaAutor.appendChild(borrarTD);
+    
+                const editarTD = document.createElement('td');
+                const imgEditar = document.createElement('img');
+                imgEditar.src = 'imagenes/Lapiz.png';
+                editarTD.appendChild(imgEditar);
+                filaAutor.appendChild(editarTD);
+    
+                tablaAutores.appendChild(filaAutor);
             });
         } else {
             console.log('No se pudo obtener la lista de Autores');
         }
     }
+    
 }
