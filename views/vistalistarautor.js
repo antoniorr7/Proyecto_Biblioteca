b5ba8ad82ccc458b4530ceea190cdacf7a230e63
Carlos <cmerinoribera@gmail.com>
2024-01-12 17:28:46 +0100

    Co-authored-by: antonioreyesrabel14@gmail.com <antonioreyesrabel14@gmail.com>

--- views/vistalistarautor.js
+++ views/vistalistarautor.js
@@ -14,49 +14,78 @@ export class VistaListarAutor extends Vista {
         const autores = await this.datos.mostrarAutor();
     
         if (autores) {
-            const tablaAutores = document.getElementById('tabla_autores');
+            const divListarAutor = document.getElementById('divListarAutor');
+            const scrollDiv = document.getElementById('scroll');
+    
+            // Limpiar la tabla existente
+            scrollDiv.innerHTML = '';
+    
+            const tablaAutores = document.createElement('table');
+            tablaAutores.classList.add('tabla');
     
             autores.forEach((autor) => {
                 const filaAutor = document.createElement('tr');
-                
-                const nombreTD = document.createElement('td');
-                nombreTD.textContent = autor.nombre;
-                filaAutor.appendChild(nombreTD);
     
-                const fechaNacimientoTD = document.createElement('td');
-                fechaNacimientoTD.textContent = autor.fecha_nacimiento;
-                filaAutor.appendChild(fechaNacimientoTD);
+                const imagenColumna = document.createElement('td');
+                imagenColumna.classList.add('imagen-columna');
     
-                const fechaMuerteTD = document.createElement('td');
-                fechaMuerteTD.textContent = autor.fecha_muerte;
-                filaAutor.appendChild(fechaMuerteTD);
+                const imagenLink = document.createElement('a');
+                imagenLink.href = 'autor.html';
     
-                const fotoTD = document.createElement('td');
-                fotoTD.textContent = autor.foto; // O puedes usar un elemento de imagen si 'foto' es la URL de la imagen
-                filaAutor.appendChild(fotoTD);
+                const imagen = document.createElement('img');
+                imagen.src = autor.foto; // Asumiendo que 'foto' es la URL de la imagen
+                imagen.alt = 'Descripción de la imagen';
     
-                const nacionalidadTD = document.createElement('td');
-                nacionalidadTD.textContent = autor.nacionalidad;
-                filaAutor.appendChild(nacionalidadTD);
+                const nombreAutorLink = document.createElement('a');
+                nombreAutorLink.href = 'autor.html';
     
-                const borrarTD = document.createElement('td');
-                const imgBorrar = document.createElement('img');
-                imgBorrar.src = 'imagenes/basura.png';
-                borrarTD.appendChild(imgBorrar);
-                filaAutor.appendChild(borrarTD);
+                const nombreAutorSpan = document.createElement('span');
+                nombreAutorSpan.classList.add('nombre-autor');
+                nombreAutorSpan.textContent = autor.nombre;
     
-                const editarTD = document.createElement('td');
-                const imgEditar = document.createElement('img');
-                imgEditar.src = 'imagenes/Lapiz.png';
-                editarTD.appendChild(imgEditar);
-                filaAutor.appendChild(editarTD);
+                nombreAutorLink.appendChild(imagen);
+                nombreAutorLink.appendChild(nombreAutorSpan);
+                imagenLink.appendChild(nombreAutorLink);
+    
+                imagenColumna.appendChild(imagenLink);
+    
+                const editorColumna = document.createElement('td');
+                editorColumna.classList.add('editor-columna');
+    
+                const editarLink = document.createElement('a');
+                editarLink.href = 'formulario-autores.html';
+    
+                const editarImagen = document.createElement('img');
+                editarImagen.src = 'imagenes/lapiz.png';
+                editarImagen.alt = 'Editar autor';
+                editarImagen.classList.add('editor');
+    
+                const eliminarLink = document.createElement('a');
+    
+                const eliminarImagen = document.createElement('img');
+                eliminarImagen.src = 'imagenes/papelera.png';
+                eliminarImagen.alt = 'Eliminar autor';
+                eliminarImagen.classList.add('editor');
+    
+                editarLink.appendChild(editarImagen);
+                eliminarLink.appendChild(eliminarImagen);
+    
+                editorColumna.appendChild(editarLink);
+                editorColumna.appendChild(eliminarLink);
+    
+                filaAutor.appendChild(imagenColumna);
+                filaAutor.appendChild(editorColumna);
     
                 tablaAutores.appendChild(filaAutor);
             });
+    
+            scrollDiv.appendChild(tablaAutores);
+            divListarAutor.appendChild(scrollDiv);
         } else {
             console.log('No se pudo obtener la lista de Autores');
-        }    
+        }
     }
+    
     
     async pulsarCrear() {
         this.controlador.verVista(Vista.vistainsertarautor)
--- views/vistalistarlibro.js
+++ views/vistalistarlibro.js
@@ -1,27 +1,41 @@
-import { Vista } from './vista.js'
-import { Modelo } from '../models/modelo.js'
+import { Vista } from './vista.js';
+import { Modelo } from '../models/modelo.js';
 
 export class VistaListarLibro extends Vista {
     constructor(controlador, base) {
-        super(controlador, base)
-        this.datos = new Modelo()
+        super(controlador, base);
+        this.datos = new Modelo();
     }
 
     async visualizarLibro() {
-        const obras = await this.datos.mostrarObra()
+        const obras = await this.datos.mostrarObra();
 
         if (obras) {
-            const divListarLibro = document.getElementById('divListarLibro')
-            divListarLibro.innerHTML = ''
+            const divListarLibro = document.getElementById('divListarLibro');
+            divListarLibro.innerHTML = '';
 
             obras.forEach((obra) => {
-                const obraElement = document.createElement('div');
-                obraElement.textContent = `Título: ${obra.titulo} | Fecha Publicacion: ${obra.fecha_publicacion} | Reseña: ${obra.reseña} | Género: ${obra.genero}`;
-                divListarLibro.appendChild(obraElement);
+                // Crear un nuevo elemento h3 para cada obra
+                const obraElement = document.createElement('h3');
+                obraElement.textContent = `Título: ${obra.titulo}`;
+                
+                // Crear una nueva estructura de card para cada obra
+                const cardElement = document.createElement('div');
+                cardElement.classList.add('card');
+                const cardContent = document.createElement('div');
+                cardContent.classList.add('card-content');
+                cardContent.appendChild(obraElement);
+                cardContent.innerHTML += `
+                    <a href=""><img id="papelera" src="imagenes/papelera.png" alt="Borrar"></a>
+                    <a href="formulario-libros.html"><img id="lapiz" src="imagenes/lapiz.png" alt="Editar"></a>
+                `;
+                cardElement.appendChild(cardContent);
+
+                // Agregar la card al divListarLibro
+                divListarLibro.appendChild(cardElement);
             });
-            
         } else {
-            console.log('No se pudo obtener la lista de obras')
+            console.log('No se pudo obtener la lista de obras');
         }
     }
 }
