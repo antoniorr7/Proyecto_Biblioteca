7692e76f19a27d7f3645003f75d7787b49a1c5f7
Carlos <cmerinoribera@gmail.com>
2024-01-22 17:41:42 +0100

    cambios

--- views/vistalistarlibro.js
+++ views/vistalistarlibro.js
@@ -3,6 +3,30 @@ import { ModeloObra } from '../models/modeloobra.js';
 import { VistaLibro } from '../views/vistalibro.js';
 import { VistaListarAutor } from '../views/vistalistarautor.js';
 
+// Función para establecer una cookie
+function setCookie(nombre, valor, dias) {
+    const fechaExpiracion = new Date();
+    fechaExpiracion.setTime(fechaExpiracion.getTime() + (dias * 24 * 60 * 60 * 1000));
+    const expira = "expires=" + fechaExpiracion.toUTCString();
+    document.cookie = nombre + "=" + valor + ";" + expira + ";path=/";
+}
+
+// Función para obtener el valor de una cookie por nombre
+function getCookie(nombre) {
+    const nombreCookie = nombre + "=";
+    const cookies = document.cookie.split(';');
+    for (let i = 0; i < cookies.length; i++) {
+        let cookie = cookies[i];
+        while (cookie.charAt(0) == ' ') {
+            cookie = cookie.substring(1);
+        }
+        if (cookie.indexOf(nombreCookie) == 0) {
+            return cookie.substring(nombreCookie.length, cookie.length);
+        }
+    }
+    return "";
+}
+
 export class VistaListarLibro extends Vista {
     constructor(controlador, base, autorseleccionado) {
         super(controlador, base);
@@ -11,11 +35,33 @@ export class VistaListarLibro extends Vista {
         this.autorseleccionado = autorseleccionado;
 
         const crear = document.getElementById('crearLibro')
-        crear.onclick = this.pulsarCrear.bind(this)
+        crear.onclick = this.pulsarCrear.bind(this);
 
-        this.obra
-        this.libroseleccionado
+        this.obra = null; // Inicializar correctamente
+        this.libroseleccionado = null; // Inicializar correctamente
     }   
+
+    toggleFavorito(idLibro, favLibro) {
+        const isFavorito = getCookie(`fav_${idLibro}`) === 'true';
+
+        if (isFavorito) {
+            this.quitarFavorito(idLibro, favLibro);
+        } else {
+            this.agregarFavorito(idLibro, favLibro);
+        }
+    }
+
+    // Función para agregar un libro a favoritos
+    agregarFavorito(idLibro, favLibro) {
+        favLibro.src = 'imagenes/favorite.png';
+        setCookie(`fav_${idLibro}`, 'true', 30);
+    }
+
+    // Función para quitar un libro de favoritos
+    quitarFavorito(idLibro, favLibro) {
+        favLibro.src = 'imagenes/favorite01.png';
+        setCookie(`fav_${idLibro}`, 'false', 30);
+    }
 
     async visualizarLibro() {
         try {
@@ -30,12 +76,10 @@ export class VistaListarLibro extends Vista {
                     const cardElement = document.createElement('div');
                     cardElement.classList.add('card');
 
-                    
-
                     const detalleLink = document.createElement('button');
                     detalleLink.classList.add('card-button');
                     detalleLink.addEventListener('click', () => {
-                       
+                        // Implementa la lógica que desees al hacer clic en la obra
                     });
     
                     // Crear el elemento de la imagen usando el campo 'portada' de la obra
@@ -44,9 +88,9 @@ export class VistaListarLibro extends Vista {
                     portadaImage.alt = 'Descripción de la imagen';
                     portadaImage.addEventListener('click', () => {
                         this.obra = obra;
-                        
                         this.pulsarObra();
                     });
+
                     // Agregar la imagen al enlace
                     detalleLink.appendChild(portadaImage);
     
@@ -60,32 +104,42 @@ export class VistaListarLibro extends Vista {
     
                     // Crear enlaces para la papelera y el lápiz
                     const eliminarObra = document.createElement('img');
-                    eliminarObra.src = 'imagenes/papelera.png';
+                    eliminarObra.src = 'imagenes/delete.png';
                     eliminarObra.alt = 'Eliminar Obra';
                     eliminarObra.id = 'papelera';
                     eliminarObra.classList.add('editor');
     
                     // Añadir un manejador de eventos al hacer clic en la papelera
                     eliminarObra.onclick = () => {
-                      
                         this.pulsarBorrar(obra.id);
                     };
     
                     const lapizLink = document.createElement('span');
                     lapizLink.href = 'formulario-libros.html';
-                    lapizLink.innerHTML = `<img id="lapiz" src="imagenes/lapiz.png" alt="Editar">`;
+                    lapizLink.innerHTML = `<img id="lapiz" src="imagenes/edit.png" alt="Editar">`;
                     lapizLink.onclick = () => {
-                      
                         this.pulsarEditar(obra);
                     };
+
+                    const favLibro = document.createElement('img');
+                    favLibro.src = getCookie(`fav_${obra.id}`) === 'true' ? 'imagenes/favorite.png' : 'imagenes/favorite01.png';
+                    favLibro.id = 'papelera';
+                    favLibro.classList.add('editor');
+                    
+                    favLibro.onclick = () => {
+                        this.toggleFavorito(obra.id, favLibro);
+                    };
+
                     // Agregar elementos al cardContent
                     cardContent.appendChild(obraElement);
                     cardContent.appendChild(eliminarObra);
                     cardContent.appendChild(lapizLink);
+                    cardContent.appendChild(favLibro);
     
                     // Agregar elementos al cardElement
                     cardElement.appendChild(detalleLink);
                     cardElement.appendChild(cardContent);
+                    cardElement.appendChild(favLibro);
     
                     // Agregar la card al divScrollLateral
                     divScrollLateral.appendChild(cardElement);
@@ -99,25 +153,24 @@ export class VistaListarLibro extends Vista {
     }
     
     async pulsarCrear() {
-        this.controlador.verVista(Vista.vistainsertarlibro)
+        this.controlador.verVista(Vista.vistainsertarlibro);
     }
 
     async pulsarBorrar(idObra) {
-        // Llamar al método borrarAutor con el id del autor
-      
         await this.datos.borrarObra(idObra);
         this.controlador.pulsarLibro();
     }
 
-    async pulsarObra(){ 
-     
-        this.libroseleccionado = this.obra.id_autor
-        const vistaListarAutor = new VistaListarAutor(this.controlador, this. base, this.obraseleccionada)
-        vistaListarAutor.visualizarAutor()
-        this.datosobra.rellenarObra(this.obra)
-        this.controlador.verVista(Vista.vistalibro)
+    async pulsarObra() { 
+        this.libroseleccionado = this.obra.id_autor;
+        const vistaListarAutor = new VistaListarAutor(this.controlador, this.base, this.obraseleccionada);
+        vistaListarAutor.visualizarAutor();
+        this.datosobra.rellenarObra(this.obra);
+        this.controlador.verVista(Vista.vistalibro);
     }
-    async pulsarEditar(obra){
-        console.log(obra)
+
+    async pulsarEditar(obra) {
+        console.log(obra);
+        // Implementa la lógica para editar la obra
     }
 }
